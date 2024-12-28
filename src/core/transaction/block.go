1de6c25c94347f9babf67e612da46f894046ec17
Kusuma <newsphinx29@gmail.com>
2024-12-28 12:59:37 +0700

    Update block.go

--- src/core/transaction/block.go
+++ src/core/transaction/block.go
@@ -21,3 +21,104 @@
 // SOFTWARE.
 
 package types
+
+import (
+	"math/big"
+	"time"
+
+	"github.com/sphinx-core/go/src/common"
+)
+
+// BlockHeader represents the metadata for a block in the blockchain.
+type BlockHeader struct {
+	Timestamp  int64    `json:"timestamp"`   // The timestamp when the block is mined
+	PrevHash   []byte   `json:"prev_hash"`   // Hash of the previous block
+	Difficulty *big.Int `json:"difficulty"`  // Difficulty level of mining the block
+	Nonce      uint64   `json:"nonce"`       // The nonce used in mining
+	TxRoot     []byte   `json:"tx_root"`     // Merkle root of the transactions in the block
+	StateRoot  []byte   `json:"state_root"`  // Merkle root of the state (EVM-like state)
+	GasLimit   *big.Int `json:"gas_limit"`   // The maximum gas that can be used in the block
+	GasUsed    *big.Int `json:"gas_used"`    // The actual gas used by the transactions
+	ParentHash []byte   `json:"parent_hash"` // The hash of the parent block (alternative to PrevHash)
+	OmmersHash []byte   `json:"ommers_hash"` // The hash of the ommers (uncle blocks)
+}
+
+// BlockBody represents the transactions and other data inside the block.
+type BlockBody struct {
+	TxList     []*Transaction `json:"tx_list"`     // A list of transactions in the block
+	OmmersHash []byte         `json:"ommers_hash"` // Hash representing ommers
+}
+
+// Block represents the entire block structure including the header and body.
+type Block struct {
+	Header BlockHeader `json:"header"` // Block metadata
+	Body   BlockBody   `json:"body"`   // Block transactions and ommers
+}
+
+// Transaction represents a single transaction within the block.
+type Transaction struct {
+	Sender    string   `json:"sender"`
+	Receiver  string   `json:"receiver"`
+	Amount    *big.Int `json:"amount"`
+	GasLimit  *big.Int `json:"gas_limit"`
+	GasPrice  *big.Int `json:"gas_price"`
+	Timestamp int64    `json:"timestamp"`
+	Nonce     uint64   `json:"nonce"`
+}
+
+// NewBlockHeader creates a new BlockHeader.
+func NewBlockHeader(prevHash []byte, difficulty *big.Int, txRoot, stateRoot []byte, gasLimit, gasUsed *big.Int, parentHash, ommersHash []byte) *BlockHeader {
+	return &BlockHeader{
+		Timestamp:  time.Now().Unix(),
+		PrevHash:   prevHash,
+		Difficulty: difficulty,
+		Nonce:      uint64(0), // Default nonce is 0, will be adjusted during mining
+		TxRoot:     txRoot,
+		StateRoot:  stateRoot,
+		GasLimit:   gasLimit,
+		GasUsed:    gasUsed,
+		ParentHash: parentHash,
+		OmmersHash: ommersHash,
+	}
+}
+
+// NewBlockBody creates a new BlockBody with a list of transactions and ommers hash.
+func NewBlockBody(txList []*Transaction, ommersHash []byte) *BlockBody {
+	return &BlockBody{
+		TxList:     txList,
+		OmmersHash: ommersHash,
+	}
+}
+
+// NewBlock creates a new Block using the given header and body.
+func NewBlock(header *BlockHeader, body *BlockBody) *Block {
+	return &Block{
+		Header: *header,
+		Body:   *body,
+	}
+}
+
+// GenerateBlockHash generates the hash of the block using the BlockHeader's fields and SphinxHash.
+func (b *Block) GenerateBlockHash() []byte {
+	// Concatenate the fields of BlockHeader and BlockBody to generate the block data
+	headerData := append(b.Header.PrevHash, b.Header.TxRoot...)
+	headerData = append(headerData, b.Header.StateRoot...)
+	headerData = append(headerData, b.Header.ParentHash...)
+	headerData = append(headerData, b.Header.OmmersHash...)
+
+	// Use the SphinxHash algorithm to generate the hash
+	return common.SpxHash(headerData)
+}
+
+// Example of a function to create a transaction
+func NewTxs(sender, receiver string, amount *big.Int, gasLimit, gasPrice *big.Int, nonce uint64) *Transaction {
+	return &Transaction{
+		Sender:    sender,
+		Receiver:  receiver,
+		Amount:    amount,
+		GasLimit:  gasLimit,
+		GasPrice:  gasPrice,
+		Timestamp: time.Now().Unix(),
+		Nonce:     nonce,
+	}
+}

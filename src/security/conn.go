81e9b3cbda2863eb56023d7203191249ba0eb11f
pscs <newsphinx29@gmail.com>
2025-06-12 23:33:24 +0700

    Update conn.go

--- src/security/conn.go
+++ src/security/conn.go
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-// go/src/security/sec_conn.go
+// go/src/security/conn.go
 package security
 
 import (

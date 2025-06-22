89a3c80b6011aa79de2951e03b7f474b6f179a5a
pscs <newsphinx29@gmail.com>
2025-06-23 00:28:52 +0700

    updated

--- src/core/sphincs/sign/backend/types.go
+++ src/core/sphincs/sign/backend/types.go
@@ -20,6 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+// go/src/core/sphincs/sign/backend/types.go
 package sign
 
 import (
--- src/core/sphincs/sign/backend/utils.go
+++ src/core/sphincs/sign/backend/utils.go
@@ -20,6 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+// go/src/core/sphincs/sign/backend/utils.go
 package sign
 
 import (
